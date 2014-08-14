64d1e85f2df51461b59da40eeaab32465e9ba184
Henning Schulz <henning.schulz@sap.com>
2014-08-14 18:04:24 +0200

    Added description extensions in CommonlyUsed<APIs/Probes/Resources>.
    Change-Id: I1b777a9a7be83050e944b3166b7ce4701ae80393
    Signed-off-by: Henning Schulz <henning.schulz@sap.com>

---
+++ org.aim.description/src/org/aim/description/extension/CommonlyUsedAPIs.java
@@ -0,0 +1,13 @@
+package org.aim.description.extension;
+
+public final class CommonlyUsedAPIs {
+
+	public static final String DATABASE_API = "Database API";
+	public static final String ENTRY_POINT_API = "Entry Point API";
+	public static final String MESSAGING_API = "Messaging API";
+	
+	private CommonlyUsedAPIs() {
+		// should not be instantiated
+	}
+	
+}
